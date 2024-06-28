9da8be9db9bbbd69f7f6418b821f353526360ee5
Varun Kanna <73306137+varun-kanna@users.noreply.github.com>
2024-06-28 16:47:00 -0700

    did remove & clear items along with clear UI state

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
 const addItem = (e) => {
 	e.preventDefault();
@@ -20,7 +22,10 @@ const addItem = (e) => {
 	const button = createButton('remove-item btn-link text-red');
 
 	li.appendChild(button);
+	// Add li to the DOM
 	itemList.appendChild(li);
+
+	checkUI();
 	itemInput.value = '';
 };
 
@@ -38,5 +43,38 @@ function createIcon(classes) {
 	return i;
 }
 
+function removeItem(e) {
+	if (e.target.parentElement.classList.contains('remove-item')) {
+		if (confirm('Are you sure?')) {
+			e.target.parentElement.parentElement.remove();
+		}
+		checkUI();
+	}
+}
+
+function clearItems() {
+	// itemList.innerHTML = ''  // This is slower
+	while (itemList.firstChild) {
+		itemList.firstChild.remove();
+	}
+	checkUI();
+}
+
+function checkUI() {
+	// Must be in here, not global. When changes are made, this is needed to work properly
+	const items = itemList.querySelectorAll('li');
+	if (items.length === 0) {
+		clearBtn.style.display = 'none';
+		itemFilter.style.display = 'none';
+	} else {
+		clearBtn.style.display = 'block';
+		itemFilter.style.display = 'block';
+	}
+}
+
 // Event Listeners
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearItems);
+
+checkUI();
